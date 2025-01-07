131184114d1474288828a645a0d9b7178299ca5a
Ojiner <dbstjdrud115@gmail.com>
2025-01-07 14:19:29 +0900

    기본커밋

---
+++ src/main/java/com/ll/JPA_Detail_2501/JpaDetail2501Application.java
@@ -0,0 +1,13 @@
+package com.ll.JPA_Detail_2501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDetail2501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDetail2501Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/JPA_Detail_2501/JpaDetail2501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.JPA_Detail_2501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDetail2501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
